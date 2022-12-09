667b0adcb236729861e63ce2cc63b0cd2eb51c55
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-08 17:19:17 -0800

    add queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,18 +2,18 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String!
-    email: String!
+    _id: ID!
+    username: String
+    email: String
     bookCount: Int
-    savedBooks: [Book]!
+    savedBooks: [Book]
   }
 
   type Book {
     bookId: String!
     authors: [String]
-    description: String!
-    title: String!
+    description: String
+    title: String
     image: String
     link: String
   }
